e0fedf5e2cf047e328a5feca9d069327de87dcbd
qinyong <1069542931@qq.com>
2020-11-13 11:40:27 +0800

    Site updated: 2020-11-13 11:40:27

--- js/scroll.js
+++ js/scroll.js
@@ -1,109 +1,137 @@
-$(function(){
-    var SPACING = 20;
-    var $toc = $('._toc');
-    var $ava = $('#avator');
-    
-    if($toc.length){
-        var minScrollTop = $toc.offset().top - SPACING;
-        var tocState = {
-            start: {
-                'position':'absolute',
-                'top':minScrollTop
-            },
-            process: {
-                'position':'fixed',
-                'top':SPACING
+(function () {
+
+    /**
+     * @description 监听滚动高度，是否显示返回顶部
+     */
+    function myCustomFn(el) {
+        if (el.mcs.top < -600) {
+            if ($('#return-top').css('opacity') === '0') {
+                $('#return-top').velocity('stop').velocity({opacity: 1}, {
+                    duration: 500,
+                    display: 'block'
+                });
             }
+        } else {
+            $('#return-top').velocity('stop').velocity({opacity: 0}, {
+                duration: 500,
+                display: 'none'
+            });
         }
-        $(window).scroll(function(){
-            var scrollTop = $(window).scrollTop();
-
-            if(scrollTop < minScrollTop){
-                $toc.css(tocState.start);
-            }
-            else{
-                $toc.css(tocState.process);
-            }
-        })
     }
 
-    if($ava.length) {
-        let minScrollTop = $ava.offset().top - 30;
-        let avaState = {
-            start: {
-                'position':'absolute',
-                'top':minScrollTop
-            },
-            process: {
-                'position':'fixed',
-                'top':SPACING
-            }            
-        }
-        $(window).scroll(function(){
-            let scrollTop = $(window).scrollTop();
-            if(scrollTop < minScrollTop) {
-                $ava.css(avaState.start);
-            }
-            else {
-                $ava.css(avaState.process);
-            }
-
-        })
-    }
-});
-$(window).resize(function(){
-    var $toc = $('._toc');
-    var minwidth = 1480;
-    var $avator = $('#avator');
-    var $postswrapper = $('.posts-wrapper');
-    if($(window).width()<1480){
-        $toc.css("display","none");
-    }
-    else {
-        $toc.css("display","block");
-    }
-    if($(window).width()<950){
-        $avator.css("display","none");
-        $postswrapper.css("width","100%");
-    }
-    else {
-        $avator.css("display","block");
-        $postswrapper.css("width","73%");
-
-    }
-});
-$(function(){
-    var $toc = $('._toc');
-    var minwidth = 1480;
-    var $avator = $('#avator');
-    var $postswrapper = $('.posts-wrapper');
-    if($(window).width<1480){
-        $toc.css("display","none");
-    }
-    if($(window).width()<950){
-        $avator.css("display","none");
-        $postswrapper.css("width","100%");
-    }
-});
-$(function(){
-    var HEADERFIX = 30;
-    var $toclink = $('.toc-link'),
-    $headerlink = $('.headerlink');
-    $(window).scroll(function(){
-        var headerlinkTop = $.map($headerlink,function(link){
-            return $(link).offset().top;
+    /**
+     * @description 阅读进度
+     * @param el
+     */
+    function readPercent(el) {
+        // console.log(el.mcs.content[0].offsetHeight);
+        let percent = Math.round(-el.mcs.top / (el.mcs.content[0].offsetHeight - el.offsetHeight) * 100);
+        $('.sidebar-toc-progress .progress-num').text(percent);
+        $('.sidebar-toc-progress-bar').velocity('stop').velocity({width: percent + '%'}, {
+            duration: 100,
+            easing: 'easeInOutQuart'
         });
-        var scrollTop = $(window).scrollTop();
-        for(var i=0;i<$toclink.length;i++){
-            var isLast = i+1 === $toclink.length,
-            currentTop = headerlinkTop[i] - HEADERFIX,
-            nextTop = isLast ? Infinity : headerlinkTop[i+1]- HEADERFIX;
-            if(currentTop < scrollTop && scrollTop <= nextTop){
-                $($toclink[i]).addClass('active');
-            }
-            else{
-                $($toclink[i]).removeClass('active');
+    }
+
+    /**
+     * @description 滚动到页面顶部
+     */
+    $('#return-top').on('click', function () {
+        $('#content-outer').mCustomScrollbar('scrollTo', 'top', {
+            scrollInertia: 1000,
+            scrollEasing: 'easeInOut'
+        });
+    });
+
+    /**
+     * @description 滚动条修饰 - jquery 插件
+     */
+    $('#content-outer').mCustomScrollbar({
+        theme: 'minimal',
+        axis: 'y', // horizontal scrollbar
+        callbacks: {
+            whileScrolling: function () {
+                myCustomFn(this);
+                readPercent(this);
+                findHeadPosition(this);
+            },
+            onScroll: function () {
+
             }
         }
     });
-});
+    $('#sidebar-toc-content').mCustomScrollbar({
+        theme: 'minimal',
+        axis: 'y', // horizontal scrollbar
+        callbacks: {}
+    });
+
+    // $('figure.highlight').mCustomScrollbar({
+    //     theme: 'minimal',
+    //     axis: 'x', // horizontal scrollbar
+    //     mouseWheel: {
+    //         enable: false
+    //     },
+    //     callbacks: {}
+    // });
+
+    /**
+     * @description menu link scroll to content
+     */
+    $('.sidebar-toc-content .toc-link').on('click', function (e) {
+        e.preventDefault();
+        let _id = $(this).attr('href');
+        $('#content-outer').mCustomScrollbar('scrollTo', _id, {
+            scrollInertia: 500
+        });
+    });
+
+    /**
+     * @description 找到当前页面的位置，更改 active 目录
+     * @param el
+     */
+    function findHeadPosition(el) {
+        let currentId = '';
+        const menuHeight = $('#menu-outer').height() + 1;
+        // console.log(el.mcs);
+        let list = $('#post').find('h1,h2,h3,h4,h5,h6');
+        list.each(function () {
+            if ($(this).offset().top <= menuHeight) {
+                currentId = $(this).attr('id');
+            }
+        });
+        $('.sidebar-toc-content .toc-link').removeClass('active');
+        if (currentId === '') {
+            // currentId = list[0].id;
+            return;
+        }
+        let $this = $('.sidebar-toc-content .toc-link[href="#' + currentId + '"]');
+        $this.addClass('active');
+        let parents = $this.parents('.toc-child');
+        if (parents.length > 0) {
+            let child = null;
+            parents.length > 1 ? child = parents.eq(parents.length - 1).find('.toc-child') : child = parents;
+            if (child.length > 0 && child.is(':hidden')) {
+                expandToc(child);
+            }
+            parents.eq(parents.length - 1).closest('.toc-item').siblings('.toc-item').find('.toc-child').hide();
+        } else {
+            if ($this.closest('.toc-item').find('.toc-child').is(':hidden')) {
+                expandToc($this.closest('.toc-item').find('.toc-child'));
+            }
+            $this.closest('.toc-item').siblings('.toc-item').find('.toc-child').hide();
+        }
+    }
+
+    /**
+     * @description expand toc-item
+     * @param $item
+     */
+    function expandToc($item) {
+        // $item.show();
+        $item.velocity('stop').velocity('fadeIn', {
+            duration: 500,
+            easing: 'easeInQuart'
+        });
+    }
+}());
